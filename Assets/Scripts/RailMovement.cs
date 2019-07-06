c7bb1433aa08126633376b3c6a66ff52e542d25f
kurtuluserturk <47227072+kurtuluserturk@users.noreply.github.com>
2019-07-06 19:47:14 +0300

    Project Added

---
+++ Assets/Scripts/RailMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMovement : MonoBehaviour
+{
+     private float speed = 4.0f;
+    
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;        
+    }   
+
+    void Update()
+    {
+                
+    }   
+}
---
+++ Assets/Scripts/SwitchMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMovement : MonoBehaviour
+{    
+    void Start()
+    {
+        
+    }
+   
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if ((collider.gameObject.name == "ball")&&(Input.GetKeyDown(KeyCode.RightArrow)))
+        {            
+            transform.Rotate(0, 0, -45);            
+        }
+        if ((collider.gameObject.name == "ball") && (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            transform.Rotate(0, 0, 45);
+        }
+    }
+}
